f4deca5ed9cbaad35093d2496589838ca2f50fb0
Ashikur Rahman <ashikrn.hq04@gmail.com>
2024-11-24 22:32:06 +0600

    Script to control video player and user interacting

---
+++ index.js
@@ -0,0 +1,96 @@
+class VideoPlay {
+  constructor(videoWrapper, playBtn) {
+    this.videoWrapper = videoWrapper;
+    this.videoElement = videoWrapper.querySelector("video");
+    this.playBtn = playBtn;
+
+    this.play = this.videoPlay.bind(this.videoElement);
+    this.restart = this.videoRestart.bind(this.videoElement);
+    this.pause = this.videoPause.bind(this.videoElement);
+    this.btnText = this.playBtnText.bind(this.playBtn, this.videoElement);
+    this.observer = this.videoObserve.bind(this.videoElement, this.btnText);
+
+    this.btnShow = this.playBtnShow.bind(this.playBtn);
+    this.btnHide = this.playBtnHide.bind(this.playBtn);
+
+    this.videoWrapper?.addEventListener("click", (e) => {
+      if (e.target == this.videoElement || e.target == this.playBtn) {
+        if (this.videoElement.muted && !this.videoElement.paused) {
+          this.restart();
+        } else if (!this.videoElement.paused && !this.videoElement.muted) {
+          this.pause();
+          this.btnShow();
+        } else {
+          this.play();
+        }
+        this.btnText();
+      } else {
+        return null;
+      }
+    });
+
+    this.videoWrapper?.addEventListener("mouseover", (e) => {
+      if (e.target == this.videoElement) this.btnShow();
+    });
+
+    this.videoWrapper?.addEventListener("mouseleave", (e) => {
+      this.btnHide();
+    });
+    this.videoElement.addEventListener("playing", () => {
+      if (!this.muted) {
+        setTimeout(this.btnHide, 1000);
+      }
+    });
+
+    this.videoElement.play();
+    this.observer();
+  }
+
+  playBtnText(videoElement) {
+    (!videoElement.paused && videoElement.muted) || videoElement.paused
+      ? (this.textContent = "Play")
+      : (this.textContent = "Pause");
+  }
+
+  playBtnShow() {
+    this.classList.add("active");
+  }
+
+  playBtnHide() {
+    this.classList.remove("active");
+  }
+
+  videoRestart() {
+    this.currentTime = 0;
+    this.play();
+    this.muted = false;
+  }
+
+  videoPause() {
+    this.pause();
+  }
+
+  videoPlay() {
+    this.play();
+  }
+
+  videoObserve(btnText) {
+    let observer = new IntersectionObserver((item) => {
+      if (!item[0].isIntersecting) {
+        if (!item[0].target.paused && !item[0].target.muted) {
+          item[0].target.muted = true;
+        } else {
+          item[0].target.muted = true;
+          item[0].target.play();
+        }
+      }
+      btnText();
+    });
+
+    observer.observe(this);
+  }
+}
+
+const playBtn = document.querySelector(".playBtn");
+const videoWrapper = document.querySelector(".video-wrapper");
+let videoPlay = new VideoPlay(videoWrapper, playBtn);
